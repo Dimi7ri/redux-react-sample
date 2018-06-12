0547e66b130b5594141697797da9f7ee588b3d5f
dimi7ri <vasiliev.dimi@gmail.com>
2018-06-12 18:37:31 +1000

    Initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Provider } from 'react-redux';
+
+import Posts from './components/Posts';
+import PostsForm from './components/Postsform';
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <PostsForm />
+          <hr/>
+          <Posts />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
